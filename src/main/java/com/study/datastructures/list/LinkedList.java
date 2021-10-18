d31143617efb9077359d35ff0f2c9ca256514f69
okotliar <okotliar@luxoft.com>
2021-10-18 13:07:02 +0300

    add LinkedList

--- src/main/java/com/study/datastructures/list/LinkedList.java
+++ src/main/java/com/study/datastructures/list/LinkedList.java
@@ -1,100 +1,267 @@
 package com.study.datastructures.list;
 
-public class LinkedList implements List {
+import java.util.StringJoiner;
 
-    private Node[] array;
+public class LinkedList implements List {
 
     private Node head;
     private Node tail;
     private int size;
 
 
-
-    public LinkedList(int capacity) {
-        this.array = new Node[capacity];
+    public void add(Object value) {
+        this.add(value, size);
     }
 
 
-    public void add(Object value) {
+    public void add(Object value, int index) {
+
+        if (index < 0 || index > size) {
+            throw new IndexOutOfBoundsException("Index should be between '0' and " + size + " (including)");
+        }
+        Node ourNode = null;
         if (size == 0) {
             head = tail = new Node(value);
-            array[0] = head;
-        } else {
-            if (size == array.length) {
-                Node[] arrayCopy = new Node[array.length + array.length / 2];
-                System.arraycopy(array, 0, arrayCopy, 0, size);
-                array = arrayCopy;
+        } else if (index == size) {
+
+            ourNode = head;
+            while (ourNode.getNext() != null) {
+                ourNode = ourNode.getNext();
             }
-            tail.setValue(value);
-            tail.setPrev(array[size - 1]);
-            tail.setNext(array[size + 1]);
+            ourNode.setNext(new Node(value, tail, null));
+            tail = ourNode.getNext();
+        } else {
+            Node node = new Node(value, this.getNode(index - 1), this.getNode(index));
+            if (index < size / 2) {
+                this.getNode(index).setPrev(node);
+                this.getNode(index - 1).setNext(node);
+            } else {
+                this.getNode(index - 1).setNext(node);
+                this.getNode(index).setPrev(node);
+            }
         }
         size++;
     }
 
 
-    public void add(Object value, int index) {
-        if (index < 0 || index > size) {
+    public Object remove(int index) {
+        if (index < 0 || index >= size) {
             throw new IndexOutOfBoundsException("Index should be between '0' and " + size + " (including)");
         }
-        if (size == 0) {
-            head = tail = new Node(value);
-            array[0] = head;
-            size++;
+        Object value = this.get(index);
+        if (index == 0) {
+            this.getNode(1).setPrev(null);
+            head = this.getNode(1);
+        } else if (index == size - 1) {
+            tail = this.getNode(size - 2);
+            tail.setNext(null);
+        } else if (size == 3 && index == 1) {
+            head.setNext(tail);
+            tail.setPrev(head);
         } else {
-            if (size == array.length) {
-                Node[] arrayCopy = new Node[array.length + array.length / 2];
-                System.arraycopy(array, 0, arrayCopy, 0, size);
-                array = arrayCopy;
+            if (index + 1 <= size / 2) {
+                this.getNode(index + 1).setPrev(this.getNode(index - 1));
+                this.getNode(index - 1).setNext(this.getNode(index + 1));
+            } else {
+                this.getNode(index - 1).setNext(this.getNode(index + 1));
+                this.getNode(index + 1).setPrev(this.getNode(index - 1));
+
             }
-//            array[index].value = value;
-//            array[index].prev = array[size - 1];
-//            array[index].next = array[size + 1];
+
         }
-    }
+        size--;
 
-
-    public Object remove(int index) {
-        return null;
+        return value;
     }
 
 
     public Object get(int index) {
-        return null;
+        return this.getNode(index).getValue();
     }
 
 
     public Object set(Object value, int index) {
-        return null;
+        if (index < 0 || index >= size) {
+            throw new IndexOutOfBoundsException("Index should be between '0' and " + size + " (including)");
+        }
+        Object oldValue = getNode(index).getValue();
+        this.getNode(index).setValue(value);
+        return oldValue;
     }
 
 
     public void clear() {
-
+        head = tail = null;
+        size = 0;
     }
 
 
     public int size() {
-        return 0;
+        return size;
     }
 
 
     public boolean isEmpty() {
+        if (head == null && tail == null) {
+            return true;
+        }
         return false;
     }
 
 
     public boolean contains(Object value) {
+        if (head.getValue().equals(value)) {
+            return true;
+        }
+        if (tail.getValue().equals(value)) {
+            return true;
+        }
+
+        Node ourNode = head;
+        while (ourNode.getNext() != null) {
+            if (ourNode.getValue().equals(value)) {
+                return true;
+            }
+            ourNode = ourNode.getNext();
+        }
         return false;
     }
 
 
     public int indexOf(Object value) {
-        return 0;
+        int index = 0;
+        Node node = head;
+        while (node.getNext() != null) {
+            if (node.getValue().equals(value)) {
+                return index;
+            }
+            node = node.getNext();
+            index++;
+        }
+        if (tail.getValue().equals(value)) {
+            return size - 1;
+        }
+        return -1;
     }
 
 
     public int lastIndexOf(Object value) {
-        return 0;
+        int index = size - 1;
+        Node node = tail;
+        while (node.getPrev() != null) {
+            if (node.getValue().equals(value)) {
+                return index;
+            }
+            node = node.getPrev();
+            index--;
+        }
+        if (head.getValue().equals(value)) {
+            return 0;
+        }
+        return -1;
+    }
+
+    public String toString() {
+
+        StringJoiner stringJoiner = new StringJoiner(", ", "[", "]");
+
+        Node ourNode = head;
+
+
+        if (ourNode != null) {
+            stringJoiner.add(String.valueOf(ourNode.getValue()));
+        }
+        while (ourNode.getNext() != null) {
+            ourNode = ourNode.getNext();
+            stringJoiner.add(String.valueOf(ourNode.getValue()));
+        }
+        return stringJoiner.toString();
+
+    }
+
+    public Node getNode(int index) {
+        if (index < 0 || index >= size) {
+            throw new IndexOutOfBoundsException("Index should be between '0' and " + size + " (including)");
+        }
+        if (index == 0) {
+            return head;
+        }
+        if (index == size - 1) {
+            return tail;
+        }
+        Node ourNode = null;
+        if (index <= size / 2) {
+            ourNode = head;
+            int count = 0;
+            while (count < index) {
+                ourNode = ourNode.getNext();
+                count++;
+            }
+        } else {
+            ourNode = tail;
+            int count = size - 1;
+            while (count > index) {
+                ourNode = ourNode.getPrev();
+                count--;
+            }
+        }
+
+        return ourNode;
+    }
+
+    private class Node {
+        private Object value;
+
+        private Node prev;
+        private Node next;
+
+        public Node(Object value) {
+            this.value = value;
+        }
+
+        public Node(Object value, Node prev, Node next) {
+            this.value = value;
+            this.prev = prev;
+            this.next = next;
+        }
+
+
+        public Object getValue() {
+            return value;
+        }
+
+        public void setValue(Object value) {
+            this.value = value;
+        }
+
+        public Node getPrev() {
+            return prev;
+        }
+
+        public void setPrev(Node prev) {
+            this.prev = prev;
+        }
+
+        public Node getNext() {
+            return next;
+        }
+
+        public void setNext(Node next) {
+            this.next = next;
+        }
+
+        @Override
+        public boolean equals(Object o) {
+            if (this == o) return true;
+            if (o == null || getClass() != o.getClass()) return false;
+            Node node = (Node) o;
+            return value.equals(node.value);
+        }
+
+
+        @Override
+        public String toString() {
+            return "" + value;
+        }
     }
 }
--- src/test/java/com/study/datastructures/list/LinkedListTest.java
+++ src/test/java/com/study/datastructures/list/LinkedListTest.java
@@ -7,127 +7,168 @@ import static org.junit.Assert.*;
 
 public class LinkedListTest {
 
-    ArrayList arrayList;
+    LinkedList linkedList;
 
     @Before
     public void setUp() throws Exception {
-        arrayList = new ArrayList();
-        arrayList.add("0");
-        arrayList.add("1");
-        arrayList.add("2");
-        arrayList.add("3");
-        arrayList.add("4");
-        arrayList.add("5");
-        arrayList.add("6");
-        arrayList.add("7");
-        arrayList.add("8");
-        arrayList.add("9");
-        arrayList.add("10");
+        linkedList = new LinkedList();
+        linkedList.add("0");
+        linkedList.add("1");
+        linkedList.add("2");
+        linkedList.add("3");
+        linkedList.add("4");
+        linkedList.add("5");
+        linkedList.add("6");
+        linkedList.add("7");
+        linkedList.add("8");
+        linkedList.add("9");
+        linkedList.add("10");
     }
 
     @Test
     public void testAdd() {
-        arrayList.add("11");
+        linkedList.add("11");
 
 
-        assertEquals(12, arrayList.size());
-        assertEquals("11", arrayList.get(11));
+        assertEquals(12, linkedList.size());
+        assertEquals("11", linkedList.get(11));
 
     }
 
     @Test
     public void testAddForIndex() {
-        arrayList.add("11", 5);
+        linkedList.add("21", 5);
+        linkedList.add("11", 11);
 
-        assertEquals(12, arrayList.size());
-        assertEquals("11", arrayList.get(5));
-        assertEquals("5", arrayList.get(6));
-        assertEquals("10", arrayList.get(11));
+        assertEquals(13, linkedList.size());
+        assertEquals("9", linkedList.get(10));
+        assertEquals("11", linkedList.get(11));
+//        assertEquals("5", linkedList.get(6));
+//        assertEquals("10", linkedList.get(11));
 
     }
 
     @Test
     public void testRemove() {
-        arrayList.remove(0);
+        linkedList.remove(0);
+        linkedList.remove(2);
+        linkedList.remove(8);
 
-        assertEquals("1", arrayList.get(0));
-        assertEquals(10, arrayList.size());
-        assertEquals("10", arrayList.get(9));
-        assertEquals("1", arrayList.remove(0).toString());
+        assertEquals("1", linkedList.get(0));
+        assertEquals(8, linkedList.size());
+        assertEquals("4", linkedList.get(2));
+        assertEquals("9", linkedList.remove(7).toString());
+
+
+    }
+
+    @Test
+    public void testRemoveFirstElementWhenListHasThreeElements() {
+
+        List list = new LinkedList();
+        list.add("22");
+        list.add("33");
+        list.add("44");
+
+        list.remove(1);
+
+        assertEquals(2, list.size());
+        assertEquals("22", list.get(0));
+        assertEquals("44", list.get(1));
 
     }
 
     @Test
     public void testGet() {
-        assertEquals("0", arrayList.get(0));
+        assertEquals("0", linkedList.get(0));
+        assertEquals("6", linkedList.get(6));
+        assertEquals("10", linkedList.get(10));
+//        assertEquals("11", linkedList.get(11));
+//        assertThrows(IndexOutOfBoundsException.class, "Index should be between '0' and 11 (including)");
+
     }
 
     @Test
     public void testSet() {
-        arrayList.set("99", 3);
+        linkedList.set("99", 3);
+        linkedList.set(null, 0);
+        linkedList.set("100", 10);
 
-        assertEquals("99", arrayList.get(3));
-        assertEquals(11, arrayList.size());
+        assertEquals("99", linkedList.get(3));
+        assertEquals(null, linkedList.get(0));
+        assertEquals("100", linkedList.get(10));
+
+        assertEquals(11, linkedList.size());
 
     }
 
     @Test
     public void testClear() {
-        arrayList.clear();
+        linkedList.clear();
 
-        assertEquals(0, arrayList.size());
-        assertNull(arrayList.get(0));
+        assertEquals(0, linkedList.size());
+//        assertNull(linkedList.get(0));
     }
 
     @Test
     public void testSize() {
-        assertEquals(11, arrayList.size());
-        arrayList.add("0");
-        assertEquals(12, arrayList.size());
-        arrayList.remove(5);
-        arrayList.remove(5);
-        assertEquals(10, arrayList.size());
+        assertEquals(11, linkedList.size());
+        linkedList.add("0");
+        assertEquals(12, linkedList.size());
+        linkedList.remove(5);
+        linkedList.remove(5);
+        assertEquals(10, linkedList.size());
 
 
     }
 
     @Test
     public void testIsEmpty() {
-        arrayList = new ArrayList();
+        linkedList = new LinkedList();
 
-        assertTrue(arrayList.isEmpty());
-        arrayList.add("1");
-        assertFalse(arrayList.isEmpty());
+        assertTrue(linkedList.isEmpty());
+        linkedList.add("1");
+        assertFalse(linkedList.isEmpty());
     }
 
     @Test
     public void testContains() {
 
-        assertTrue(arrayList.contains("5"));
+        assertTrue(linkedList.contains("5"));
+        assertTrue(linkedList.contains("0"));
+        assertTrue(linkedList.contains("10"));
+        assertFalse(linkedList.contains("15"));
     }
 
     @Test
     public void testIndexOf() {
-        assertEquals(2, arrayList.indexOf("2"));
 
-        arrayList.add(new String("3"), 3);
+        linkedList.set("2", 4);
+
+        assertEquals(0, linkedList.indexOf("0"));
+        assertEquals(2, linkedList.indexOf("2"));
+        assertEquals(-1, linkedList.indexOf("20"));
+        assertEquals(10, linkedList.indexOf("10"));
     }
 
     @Test
     public void testLastIndexOf() {
-        arrayList.add("4");
+        linkedList.set("4", 8);
 
-        assertEquals(11, arrayList.lastIndexOf("4"));
+        assertEquals(8, linkedList.lastIndexOf("4"));
+        assertEquals(0, linkedList.lastIndexOf("0"));
+        assertEquals(10, linkedList.lastIndexOf("10"));
+        assertEquals(-1, linkedList.lastIndexOf("20"));
     }
 
     @Test
     public void testToString() {
-        arrayList = new ArrayList();
-        arrayList.add("1");
-        arrayList.add("2");
-        arrayList.add("3");
+        linkedList = new LinkedList();
+        linkedList.add("1");
+        linkedList.add("2");
+        linkedList.add("3");
 
 
-        assertEquals("[1, 2, 3]", arrayList.toString());
+        assertEquals("[1, 2, 3]", linkedList.toString());
     }
 }
